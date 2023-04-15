92e2a0ae5c809789fab34ef390c508b9782cffce
[Silverainox] <serainox@gmail.com>
2023-04-14 22:41:16 -0400

    Add files via upload

---
+++ YT-DLP/1-extract-wav.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create 'Downloads' folder inside script's directory
+downloads_folder="$(dirname "$(readlink -f "$0")")/Downloads"
+mkdir -p "$downloads_folder"
+
+# Check if config.json exists, otherwise generate it with default folder path
+config_file="config.json"
+if [ ! -e "$config_file" ]; then
+  echo '{ "folder": "'"$downloads_folder"'" }' > "$config_file"
+  echo "config.json file has been generated with the default folder path: $downloads_folder"
+fi
+
+# Read folder path from config.json
+folder=$(jq -r .folder "$config_file")
+
+# Prompt the user to enter the URL
+echo "Please paste the URL:"
+read url
+
+# Execute yt-dlp command with the provided URL and folder path
+yt-dlp -o "$folder/%(title)s.%(ext)s" -i --extract-audio --audio-format wav "$url"
---
+++ YT-DLP/README.md
@@ -0,0 +1,5 @@
+# YT-DLP 4 SZMELC COMMANDER
+## `1-extract-wav.sh` ~ Extract WAV from URL media
+## `2-download-mp4.sh` ~ Download media as MP4
+
+## `"folder": Downloads` ~ Creates and works inside `Downloads` folder.
